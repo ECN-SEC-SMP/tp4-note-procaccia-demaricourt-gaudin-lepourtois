53338f43cf199c5b5acb2df2d5afb2d7c36da7d8
Widelx <ahz45bny@gmail.com>
2024-01-21 19:41:53 +0100

    Minor reviews

--- Parcelle.hpp
+++ Parcelle.hpp
@@ -1,27 +1,36 @@
 #pragma once
-#include <string.h>
+
+#include <string>
+#include <vector>
+
 #include "Polygone.hpp"
 
-/*============================*/
-/*         Parcelle()         */
-/*============================*/
-class Parcelle{
-    private:
-        int numero;
-        string proprietaire;
-        float surface;
-        Polygone<int> forme;
-        int pConstructible;
-    public:
-        Parcelle(int num, string prop,  Polygone<int> forme);
-        Parcelle(Parcelle& parc);
-        int getNumero(void) const;
-        string getProprietaire(void) const;
-        float getSurface(void) const;
-        Polygone<int> getForme(void) const;
-        void setNumero(int n);
-        void setProprietaire(string prop);
-        void setForme(Polygone<int> _forme);
+using namespace std;
 
-        friend ostream& operator<<(ostream& s, Parcelle const& p);
+class Parcelle
+{
+private:
+    int numero;
+    string proprietaire;
+    float surface;
+    Polygone<float> forme;
+    int pConstructible;
+
+public:
+    /* Constructors */
+    Parcelle(int num, string prop, Polygone<float> forme);
+    Parcelle(Parcelle &other);
+
+    /* Getters */
+    int getNumero(void) const;
+    string getProprietaire(void) const;
+    float getSurface(void) const;
+    Polygone<float> getForme(void) const;
+    int getPConstructible(void) const;
+
+    /* Setters */
+    void setNumero(int n);
+    void setProprietaire(string prop);
+    void setForme(Polygone<float> forme);
+    void setPConstructible(int pConstructible);
 };
