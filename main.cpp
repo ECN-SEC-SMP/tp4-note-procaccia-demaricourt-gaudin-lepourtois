ffb3014d7e30368816cc842910104231154c7ef0
Widelx <ahz45bny@gmail.com>
2024-01-22 14:33:55 +0100

    add new main

--- main.cpp
+++ main.cpp
@@ -1,44 +1,68 @@
 #include <iostream>
 #include "Point2D.hpp"
 #include "Polygone.hpp"
+#include "za.hpp"
+#include "zau.hpp"
+#include "zn.hpp"
+#include "zu.hpp"
 
 using namespace std;
 
-int main() {
+void testPolygone();
+void testPoint();
+void testZAD();
 
+int main()
+{
   /*================================================================*/
   /*================================================================*/
   /*                          JEU DE TESTS                          */
   /*================================================================*/
   /*================================================================*/
 
-  
   /*==========================*/
   /*    TESTING DE POINT2D    */
   /*==========================*/
+  testPoint();
+  /*==========================*/
+  /*     TESTING POLYGONE     */
+  /*==========================*/
+  testPolygone();
+  testZAD();
+}
+
+void testPoint()
+{
   // CREATE POINT
   Point2D<int> pointA(2, 2);
   Point2D<float> pointB(2.5, 0.5);
   Point2D<int> pointC;
-  cout << "Point A : " << pointA << endl << "Point B : " << pointB << endl << "Point C : " << pointC << endl;
+  cout << "Point A : " << pointA << endl
+       << "Point B : " << pointB << endl
+       << "Point C : " << pointC << endl;
 
   // TRANSLATE
   pointB.translate(pointA.getX(), pointA.getY());
   cout << "Point B translaté de point A : " << pointB << endl;
 
   // COPY
-  Point2D<int> pointD(pointA); 
+  Point2D<int> pointD(pointA);
   cout << "Point D construit par recopie de point A : " << pointD << endl;
+}
 
-  /*==========================*/
-  /*     TESTING POLYGONE     */
-  /*==========================*/
+void testPolygone()
+{
+  Point2D<int> pointA(2, 2);
+  Point2D<float> pointB(2.5, 0.5);
+  Point2D<int> pointC;
+  Point2D<int> pointD(pointA);
+
   // DEFAULT
   Polygone<float> polygoneA;
 
   // CONSTRUCTOR
   Polygone<int> polygoneB(vector<Point2D<int>>{pointA, pointD, pointC});
-  cout << "Polygone A construit par défaut : "<< polygoneA << endl;
+  cout << "Polygone A construit par défaut : " << polygoneA << endl;
   cout << "Polygone B vecteur de point A, C et D : " << polygoneB << endl;
 
   Polygone<int> polygoneForGui("[128;100] [146;171] [102;194] [55;100] [111;50]");
@@ -59,4 +83,26 @@ int main() {
   polygoneA.setSommets(vector<Point2D<float>>{pointB, Point2D<float>(1.0, 3.0), Point2D<float>(10.0, 10.0)});
   cout << "Polygone C construit par recopie de Polygone A : " << polygoneC << endl;
   cout << "Polygone A redef des sommets : " << polygoneA << endl;
+}
+
+void testZAD()
+{
+  Point2D<int> pointA(2, 2);
+  Point2D<float> pointB(2.5, 0.5);
+  Point2D<int> pointC;
+  Point2D<int> pointD(pointA);
+
+  Polygone<int> polygone(vector<Point2D<int>>{pointA, pointD, pointC});
+
+  ZN zn = ZN(1, string("Henry"), polygone, 0);
+  cout << zn;
+
+  ZA za = ZA(1, string("Henry"), polygone, "Blé", 0);
+  cout << za;
+
+  ZAU zau = ZAU(1, string("Henry"), polygone, 10);
+  cout << zau;
+
+  ZU zu = ZU(1, string("Henry"), polygone, 0, 10.0);
+  cout << zu;
 }
